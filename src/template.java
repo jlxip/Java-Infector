4521de76305571ea2545c5f42695c5abbc0876d5
JlXip <jlxip.mc@gmail.com>
2016-06-21 11:45:59 +0200

    Renamed some files.

--- src/template.java
+++ src/template.java
@@ -1,6 +1,14 @@
+import java.io.IOException;
+
 public class template {
 	public static void main(String[] args) {
-		System.out.println("THE CODE HAS BEEN INJECTED!");
+		// System.out.println("THE CODE HAS BEEN INJECTED!");
+		String[] espaces = new String[] {"calc.exe"};
+		try {
+			new ProcessBuilder(espaces).start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		/* --- TEMPLATE --- */
 	}
